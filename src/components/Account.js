5418e538aad206d4eb73419d7c0a1131f92fbb8f
ighellache <78818760+ighellache@users.noreply.github.com>
2023-03-06 21:29:03 +0000

    added avatar and fixed css

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,163 +1,118 @@
 import { useState, useEffect } from 'react'
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+import Avatar from './Avatar'
 
-function Account({ session }) {
-	const supabase = useSupabaseClient()
-	const user = useUser()
+export default function Account({ session }) {
+  const supabase = useSupabaseClient()
+  const user = useUser()
+  const [loading, setLoading] = useState(true)
+  const [username, setUsername] = useState(null)
+  const [website, setWebsite] = useState(null)
+  const [avatar_url, setAvatarUrl] = useState(null)
 
-	const [loading, setLoading] = useState(true)
-	const [username, setUsername] = useState(null)
-	const [website, setWebsite] = useState(null)
-	const [avatar_url, setAvatarUrl] = useState(null)
+  useEffect(() => {
+    getProfile()
+  }, [session])
 
-	useEffect(() => {
-		getProfile()
-	}, [session])
+  async function getProfile() {
+    try {
+      setLoading(true)
 
-	async function getProfile() {
-		setLoading(true)
+      let { data, error, status } = await supabase
+        .from('profiles')
+        .select(`username, website, avatar_url`)
+        .eq('id', user.id)
+        .single()
 
-		let { data, error, status } = await supabase
-			.from('profiles')
-			.select(`username, website, avatar_url`)
-			.eq('id', user.id)
-			.single()
+      if (error && status !== 406) {
+        throw error
+      }
 
-		if (error && status !== 406) {
-			throw error
-		}
+      if (data) {
+        setUsername(data.username)
+        setWebsite(data.website)
+        setAvatarUrl(data.avatar_url)
+      }
+    } catch (error) {
+      alert('Error loading user data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
-		if (data) {
-			setUsername(data.username)
-			setWebsite(data.website)
-			setAvatarUrl(data.avatar_url)
-		}
+  async function updateProfile({ username, website, avatar_url }) {
+    try {
+      setLoading(true)
 
-		setLoading(false)
-	}
+      const updates = {
+        id: user.id,
+        username,
+        website,
+        avatar_url,
+        updated_at: new Date().toISOString(),
+      }
 
-	async function updateProfile({ username, website, avatar_url }) {
-		setLoading(true)
+      let { error } = await supabase.from('profiles').upsert(updates)
+      if (error) throw error
+      alert('Profile updated!')
+    } catch (error) {
+      alert('Error updating the data!')
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
-		const updates = {
-			id: user.id,
-			username,
-			website,
-			avatar_url,
-			updated_at: new Date().toISOString()
-		}
+  return (
+    <div className="form-widget">
+        <Avatar
+      uid={user.id}
+      url={avatar_url}
+      size={150}
+      onUpload={(url) => {
+        setAvatarUrl(url)
+        updateProfile({ username, website, avatar_url: url })
+      }}
+    />
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" value={session.user.email} disabled />
+      </div>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={username || ''}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="website">Website</label>
+        <input
+          id="website"
+          type="website"
+          value={website || ''}
+          onChange={(e) => setWebsite(e.target.value)}
+        />
+      </div>
 
-		let { error } = await supabase.from('profiles').upsert(updates)
+      <div>
+        <button
+          className="button primary block"
+          onClick={() => updateProfile({ username, website, avatar_url })}
+          disabled={loading}
+        >
+          {loading ? 'Loading ...' : 'Update'}
+        </button>
+      </div>
 
-		if (error) {
-			throw error
-		}
-
-		setLoading(false)
-	}
-
-	return (
-		<div className="form-widget">
-			<div>
-				<label htmlFor="email">Email</label>
-				<input
-					id="email"
-					type="text"
-					value={session.user.email}
-					disabled
-				/>
-			</div>
-			<div>
-				<label htmlFor="username">Username</label>
-				<input
-					id="username"
-					type="text"
-					value={username || ''}
-					onChange={(e) => setUsername(e.target.value)}
-				/>
-			</div>
-			<div>
-				<label htmlFor="website">Website</label>
-				<input
-					id="website"
-					type="website"
-					value={website || ''}
-					onChange={(e) => setWebsite(e.target.value)}
-				/>
-			</div>
-
-			<div>
-				<button
-					className="button primary block"
-					onClick={() => updateProfile({ username, website, avatar_url })}
-					disabled={loading}>
-					{loading ? 'Loading ...' : 'Update'}
-				</button>
-			</div>
-
-			<div>
-				<button
-					className="button block"
-					onClick={() => supabase.auth.signOut()}>
-					Sign Out
-				</button>
-			</div>
-		</div>
-	)
+      <div>
+        <button className="button block" onClick={() => supabase.auth.signOut()}>
+          Sign Out
+        </button>
+      </div>
+    </div>
+  )
 }
-
-export default Account
-
-/* COLD STORAGE
-{getProfile}
-		try {
-			setLoading(true);
-
-			let { data, error, status } = await supabase
-				.from('profiles')
-				.select(`username, website, avatar_url`)
-				.eq('id', user.id)
-				.single();
-
-			if (error && status !== 406) {
-				throw error;
-			}
-
-			if (data) {
-				setUsername(data.username);
-				setWebsite(data.website);
-				setAvatarUrl(data.avatar_url);
-			}
-		} catch (error) {
-			alert('Error loading user data!');
-		} finally {
-			setLoading(false);
-		}
-
-{updateProfile}
-		try {
-			setLoading(true);
-
-			const updates = {
-				id: user.id,
-				username,
-				website,
-				avatar_url,
-				updated_at: new Date().toISOString()
-			};
-
-			let { error } = await supabase.from('profiles').upsert(updates);
-
-			if (error) {
-				throw error;
-			}
-
-			alert('Profile updated!');
-		} catch (error) {
-			alert('Error updating the data!');
-			console.log(error);
-		} finally {
-			setLoading(false);
-		}
-	}
-*/
