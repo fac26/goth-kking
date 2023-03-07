fc2895a8131b8df4206016a7de2d97f4bbe3358b
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-03-07 17:31:38 +0000

    skeleton

---
+++ src/components/Auth/SignInWithOtp.js
@@ -0,0 +1,18 @@
+// import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+
+async function SignInWithOtp() {
+	// const session = useSession()
+	// const supabase = useSupabaseClient()
+	// const { data, error } = await supabase.auth.signInWithOtp({
+	// 	email: '',
+	// 	options: {
+	// 		emailRedirectTo: 'http://localhost:3000/home' // || VERCEL URL
+	// 	}
+	// })
+	// if (error) {
+	// 	console.log('error', error)
+	// 	return
+	// }
+}
+
+export default SignInWithOtp
---
+++ src/pages/login.js
@@ -0,0 +1,7 @@
+async function Login() {
+	// const session = useSession()
+	// const supabase = useSupabaseClient()
+	// return <div className="container"></div>
+}
+
+export default Login
