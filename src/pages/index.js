917bf865fdbc239b77b7dd634876fea62612bac1
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-03-07 17:29:40 +0000

    landing page v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,51 @@
-import { useRouter } from 'next/navigation' //added to redirect
+import Image from 'next/image'
+import Link from 'next/link'
 
 function LandingPage() {
-	const router = useRouter() //call router
-
 	return (
 		<div className="container">
-			<div>Info about GOTH</div>
-			<button
-				onClick={() => {
-					router.push('/login')
-				}}>
-				Login
-			</button>
+			<div>
+				<div className="">
+					<h1>Welcome to Guardian of the House!</h1>
+				</div>
+				<div className="">
+					<h2>What is Guardian of the House?</h2>
+					<p>
+						Guardian of the House is a web application that allows you to create
+						a space for members to organize and collaborate around tasks and
+						chores.
+					</p>
+				</div>
+				<div className="">
+					<h2>How do I get started?</h2>
+					<p>
+						To get started, you can either login or create an account. Once you
+						have an account, you can create a space for members to use.
+					</p>
+
+					<p>
+						Once you have created a space, you can invite members members to
+						join the space. You can create tasks and assign them to members. By
+						default, the tasks will rotate between members on a weekly basis.
+					</p>
+				</div>
+				<div className="">
+					<Image
+						src=""
+						alt="Guardian of the House"
+					/>
+				</div>
+				<div>
+					<p>Click here to</p>
+					<button>
+						{/* <Link href="./login">login with an existing account</Link> */}
+					</button>
+					<p>or</p>
+					<button>
+						{/* <Link href="./sign-up">create an account</Link> */}
+					</button>
+				</div>
+			</div>
 		</div>
 	)
 }
