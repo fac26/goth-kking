ab9c4427b4725101c2b1064564edec85e5d5a770
ighellache <78818760+ighellache@users.noreply.github.com>
2023-03-08 12:20:45 +0000

    storingdesign

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,10 +3,12 @@ import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
 
+
 function MyApp({ Component, pageProps }) {
 	const [supabase] = useState(() => createBrowserSupabaseClient())
 
 	return (
+		
 		<SessionContextProvider
 			supabaseClient={supabase}
 			initialSession={pageProps.initialSession}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import { Auth } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import Account from '@components/Account'
-
+//import layout
 function Home() {
 	const session = useSession()
 	const supabase = useSupabaseClient()
 
 	return (
+//layout insert
 		<div className="container">
 			{!session ? (
 				<Auth
