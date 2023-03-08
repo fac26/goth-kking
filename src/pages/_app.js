f5061ea5a8d841ec1c0749bca597cb5a2b25f021
Konstantina Katmada <konstantinakatmada@gmail.com>
2023-03-08 09:12:51 +0000

    add navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '@styles/globals.css'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
+import Navbar from '@components/Navbar';
 
 function MyApp({ Component, pageProps }) {
 	const [supabase] = useState(() => createBrowserSupabaseClient())
@@ -10,6 +11,7 @@ function MyApp({ Component, pageProps }) {
 		<SessionContextProvider
 			supabaseClient={supabase}
 			initialSession={pageProps.initialSession}>
+			<Navbar />
 			<Component {...pageProps} />
 		</SessionContextProvider>
 	)
