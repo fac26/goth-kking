df2ea62c6fb689253e775922e1a47afc439028be
ighellache <78818760+ighellache@users.noreply.github.com>
2023-03-08 12:43:06 +0000

    spacelist file refactored because queries are in supabase instead

--- src/components/SpaceList.js
+++ src/components/SpaceList.js
@@ -1,59 +1,116 @@
+//Version 2.0
 import { useState, useEffect } from 'react'
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
 
 function SpaceList({ session }) {
-	const supabase = useSupabaseClient()
-	const user = useUser()
+    const supabase = useSupabaseClient()
+    const user = useUser()
 
-	const [loading, setLoading] = useState(true)
-	const [spaces, setSpaces] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [spaces, setSpaces] = useState([])
 
-	//1. need to check if the user's name === member_email
-	//2. then we can use the member_email to check what spaces they created using the space_id in the space_members table
-	//3. then we use the space_id and connect to spaces(id) to retrieve the name in the spaces table
+    //1. need to check if the user's name === member_email
+    //2. then we can use the member_email to check what spaces they created using the space_id in the space_members table
+    //3. then we use the space_id and connect to spaces(id) to retrieve the name in the spaces table
 
-	useEffect(() => {
-		getSpaceList()
-	}, [session])
+    useEffect(() => {
+        getSpaceList()
+    }, [session])
 
-	async function getSpaceList() {
-		let spaceResponse = await supabase
-			.from('space_members')
-			.select('space_id') //?
-			.eq('member_email', user.email)
-		console.log(spaceResponse.data, ' from home') //array of objects
-		//0: {space_id: 36} 1	: {space_id: 37} 2: {space_id: 38}
+    async function getSpaceList() {
+        //we pass second second arg to rpc {email:user.email}, this is how we can add args to function we defined as get_spaces(email)
+        const currentUserMemberOfList = await supabase.rpc('get_spaces', {
+            email: user.email
+        })
 
-		console.log(spaceResponse.data)
-		const spaceNameList = []
-		for (let spaceObj of spaceResponse.data) {
-			let whatspaces = await supabase
-				.from('spaces')
-				.select('name')
-				.eq('id', spaceObj.space_id)
-			spaceNameList.push(whatspaces.data[0].name)
-			console.log(spaceObj.space_id)
-			console.log(whatspaces.data[0].name)
-		}
-		console.log(spaceNameList) //['yep', 'cheese']
-		setSpaces(spaceNameList)
-		console.log(spaces)
-	}
-	console.log(spaces)
-	// const linkingspaces = await supabase //should we query for each space_id with different query as we get an array?
+        console.log(currentUserMemberOfList.data, user.email)
 
-	// .from("spaces")
-	// .select(`name, space_id`)
+        const spaceNames = currentUserMemberOfList.data.map(
+            (spaceObj) => spaceObj.name
+        )
+        console.log(spaceNames)
 
-	return (
-		<div className="form-widget">
-			<ul>
-				{spaces.map((spaceitem, index) => (
-					<li key={index}>{spaceitem}</li>
-				))}
-			</ul>
-		</div>
-	)
+        setSpaces(spaceNames)
+    }
+
+    // const linkingspaces = await supabase //should we query for each space_id with different query as we get an array?
+
+    // .from("spaces")
+    // .select(name, space_id)
+
+    return (
+        <div className="form-widget">
+            <ul>
+                {spaces.map((spaceitem, index) => (
+                    <li key={index}>{spaceitem}</li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default SpaceList
+
+
+
+
+//Version 1.0
+// import { useState, useEffect } from 'react'
+// import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
+
+// function SpaceList({ session }) {
+// 	const supabase = useSupabaseClient()
+// 	const user = useUser()
+
+// 	const [loading, setLoading] = useState(true)
+// 	const [spaces, setSpaces] = useState([])
+
+// 	//1. need to check if the user's name === member_email
+// 	//2. then we can use the member_email to check what spaces they created using the space_id in the space_members table
+// 	//3. then we use the space_id and connect to spaces(id) to retrieve the name in the spaces table
+
+// 	useEffect(() => {
+// 		getSpaceList()
+// 	}, [session])
+
+// 	async function getSpaceList() {
+// 		let spaceResponse = await supabase
+// 			.from('space_members')
+// 			.select('space_id') //?
+// 			.eq('member_email', user.email)
+// 		console.log(spaceResponse.data, ' from home') //array of objects
+// 		//0: {space_id: 36} 1	: {space_id: 37} 2: {space_id: 38}
+
+// 		console.log(spaceResponse.data)
+// 		const spaceNameList = []
+// 		for (let spaceObj of spaceResponse.data) {
+// 			let whatspaces = await supabase
+// 				.from('spaces')
+// 				.select('name')
+// 				.eq('id', spaceObj.space_id)
+// 			spaceNameList.push(whatspaces.data[0].name)
+// 			console.log(spaceObj.space_id)
+// 			console.log(whatspaces.data[0].name)
+// 		}
+// 		console.log(spaceNameList) //['yep', 'cheese']
+// 		setSpaces(spaceNameList)
+// 		console.log(spaces)
+// 	}
+// 	console.log(spaces)
+// 	// const linkingspaces = await supabase //should we query for each space_id with different query as we get an array?
+
+// 	// .from("spaces")
+// 	// .select(`name, space_id`)
+
+// 	return (
+// 		<div className="form-widget">
+// 			<ul>
+// 				{spaces.map((spaceitem, index) => (
+// 					<li key={index}>{spaceitem}</li>
+// 				))}
+// 			</ul>
+// 		</div>
+// 	)
+// }
+
+// export default SpaceList
