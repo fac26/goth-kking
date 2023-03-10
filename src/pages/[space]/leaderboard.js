98f3bf2b737ea4a4410e8b6df9f5eab17e649ca7
Kr33L <105356599+Kr33L@users.noreply.github.com>
2023-03-10 17:09:36 +0000

    display leaderboard data on the page

--- src/pages/[space]/leaderboard.js
+++ src/pages/[space]/leaderboard.js
@@ -1,18 +1,46 @@
-import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
-import { useUser } from '@supabase/auth-helpers-react'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Layout from 'components/Layout'
 
-export default function LeaderboardPage() {
-	const user = useUser()
+function Leaderboard() {
+	const [members, setMembers] = useState([])
 	const supabase = useSupabaseClient()
 	const router = useRouter()
 	const path = router.asPath.slice(1)
 	const pathArr = router.asPath.split('/')
-	console.log(path, pathArr, user)
+
+	console.log
+	useEffect(() => {
+		fetchMembers()
+	}, [])
+
+	async function fetchMembers() {
+		const { data, error } = await supabase
+			.from('space_members')
+			.select('member_nickname', 'points')
+			.order('points', { ascending: false })
+
+		if (error) {
+			console.log(error)
+		} else {
+			setMembers(data)
+		}
+	}
+
 	return (
 		<Layout id={pathArr[1]}>
-			<div>Loading...LeaderboardPage</div>
+			<div>
+				<ol>
+					{members.map((member) => (
+						<li key={member.member_nickname}>
+							{member.member_nickname} - {member.points}
+						</li>
+					))}
+				</ol>
+			</div>
 		</Layout>
 	)
 }
+
+export default Leaderboard
